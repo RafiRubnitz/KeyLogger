4181908f82028d6b7ca96b4d3de7e526c73793a6
rafi <rr0556750024@gmail.com>
2025-02-10 11:11:34 +0200

    key logger

---
+++ key_logger.py
@@ -0,0 +1,44 @@
+import keyboard
+
+class KeyLogger:
+
+    def __init__(self):
+        self.data = None
+        self.path = "log.txt"
+        self.url = "My url"
+        self.listen = self.Listen()
+
+    class Listen:
+
+        def __init__(self):
+            pass
+
+        def _get_keyword(self):
+            pass
+
+        def _get_active_window(self):
+            pass
+
+        def _get_time(self):
+            pass
+
+
+    def _encrypt(self):
+        pass
+
+    def _save_in_file(self):
+        pass
+
+    def _upload_to_server(self):
+        pass
+
+    def _exit(self):
+        pass
+
+
+
+
+
+
+
+
